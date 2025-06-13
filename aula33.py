bdb16bef8a1a0f78df3624fbace13fffa6e02235
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-06-13 09:46:30 -0300

    feat: add __init__ to aula33_package

--- aula33.py
+++ aula33.py
@@ -1,5 +1,5 @@
 from sys import path
 
-from aula33_package.modulo import soma_do_modulo
+from aula33_package import soma_do_modulo
 
-print(soma_do_modulo(1,2))
+print(soma_do_modulo.dobra(2))
---
+++ aula33_package/__init__.py
@@ -0,0 +1,8 @@
+print('Você importou o', __name__)
+
+def dobra(x):
+    return x * 2
+
+from aula33_package.modulo import soma_do_modulo
+
+
