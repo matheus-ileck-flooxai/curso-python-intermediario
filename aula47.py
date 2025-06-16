f32309916233f579628e9d310821442512297985
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-06-16 11:46:10 -0300

    feat: add example of file creation using context manager

---
+++ aula47.py
@@ -0,0 +1,11 @@
+# Ambientes Virtuais em Python (venv)
+# Um ambiente virtual carrega toda a sua instalação
+# do Python para uma pasta no caminho escolhido.
+# Ao ativar um abiente virtual, a instalação do
+# Ambiente virtual será usada.
+# venv é o modulo que vamos usar para
+# criar ambientes virtuais.
+# Você pode dar o nome que preferir para um
+# Ambiente virtual, mas os mais comums são:
+# venv env .venv. env
+
